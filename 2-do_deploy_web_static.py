948822e0e492433651e8727e27807c891c943a35
chimezie1283 <agbo.chimezie1283@gmail.com>
2024-01-14 20:22:34 -0800

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,52 @@
 #!/usr/bin/python3
-"""[Fabric script]
 """
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-from fabric.api import local
-from datetime import datetime
-from os import path
-
-env.hosts = ['34.75.254.10', '34.75.125.125']
-env.user = "ubuntu"
+a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using the function do_deploy
+"""
 
 
-def do_pack():
-    """[generates a .tgz archive]
-    """
-    time_format = '%Y%m%d%H%M%S'
-    time = datetime.utcnow().strftime(time_format)
-    filepath = "versions/web_static_{}.tgz".format(time)
-    local("mkdir -p versions")
-    local("tar -cvzf {} web_static".format(filepath))
-    if path.exists(filepath):
-        return filepath
-    else:
-        return None
+from fabric.api import *
+from os.path import exists
+from os import getenv, environ
+
+env.hosts = ['100.25.165.191', '3.83.245.148']
+env.user = 'ubuntu'
+env.key_filename = '/home/root/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """[distributes an archive to your web servers]
+    """Deploys the web static to the server"""
+    if not exists(archive_path):
+        print("path does not exist\n")
+        return False
 
-    Args:
-        archive_path ([path]): [path of the archive file]
-    """
-    if not path.exists(archive_path) and path.isfile(archive_path):
-        return False
-    filename = archive_path.split("/")[-1].split(".")[0]
-    path1 = "/data/web_static/releases/{}/web_static/*".format(filename)
     try:
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
-            format(filename, filename))
-        run("sudo rm /tmp/{}.tgz".format(filename))
-        run("sudo mv {} /data/web_static/releases/{}/".format(path1, filename))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".
-            format(filename))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
+        archive_name = archive_path.split('/')[-1]
+        file_name = archive_name.split('.')[0]
+        sym_link = "/data/web_static/current"
+        release_version = f"/data/web_static/releases/{file_name}/"
+
+        # deploying locally
+        run_locally = getenv("run_locally", None)
+        if run_locally is None:
+            print(f"Deploying new_version from {archive_path}")
+            local(f"sudo mkdir -p {release_version}")
+            local(f"sudo tar -xzf {archive_path} \
+-C {release_version} --strip-components=1")
+            local(f"sudo rm -f {sym_link}")
+            local(f"sudo ln -s {release_version} {sym_link}")
+            environ['run_locally'] = "True"
+            print("Deployed locally\n")
+
+        put(archive_path, f"/tmp/{archive_name}")
+        run(f"mkdir -p {release_version}")
+        run(f"tar -xzf /tmp/{archive_name} \
+-C {release_version} --strip-components=1")
+        run(f"rm /tmp/{archive_name}")
+        run(f"rm -f {sym_link}")
+        run(f"ln -s {release_version} {sym_link}")
+        print(f"New Version Deployed --> {release_version}")
         return True
-    except:
+    except Exception as e:
+        print(f"Failed to Deploy New Version --> {release_version}\n{str(e)}")
         return False
-    return True
